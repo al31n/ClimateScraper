e1e8e97492460b70e1e783f962df188aef6437fd
andyleung <a.leung1994@gmail.com>
2015-06-15 14:06:52 -0700

    Changed parameter passing to sys.argv

--- ConvertToCSV.py
+++ ConvertToCSV.py
@@ -2,13 +2,18 @@ from pymongo import MongoClient, ASCENDING
 import unicodecsv as csv
 import json
 import os
+import sys
 
 MONGO_CONNECTION = 'localhost'
 MONGO_PORT = 27017
 MONGO_NAME = 'climateData'
 COLLECTION_MONTHLY = 'monthlyData'
 
-stationID = raw_input("Enter Station ID...")
+if len(sys.argv) is not 2 :
+	print "Invalid arguments: %s" % sys.argv
+	sys.exit()
+
+stationID = sys.argv[1]
 
 client = MongoClient(MONGO_CONNECTION, MONGO_PORT)
 db = client[MONGO_NAME]
